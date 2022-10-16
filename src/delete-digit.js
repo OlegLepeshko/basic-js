1bd2d4d95427673f49adcc7bc928be1f63a6a366
OlegLepeshko <74596871+OlegLepeshko@users.noreply.github.com>
2022-10-16 18:07:42 +0300

    feat: add encode-line

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,8 +12,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function deleteDigit(n) {
-  var s = String(n).split('').map(Number)
-  return Math.max(...s.map((el,i)=>{var r = s.slice(); r.splice(i,1); return Number( r.join(''))}))
+  let s = String(n).split('').map(Number)
+  return Math.max(...s.map((el,i)=>{let r = s.slice(); r.splice(i,1); return Number( r.join(''))}))
 
 }
 
